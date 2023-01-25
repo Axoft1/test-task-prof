4069f0e2b85d96dd7a52b9890ea337936f512e40
Aleksei Anufriev <axoft10@gmail.com>
2023-01-25 14:35:09 +0700

    first commit

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+et -e
+
+npm run build
+
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# git push -f https://github.com/axoft1/test-tsk-prof.git master:gh-pages
+git push -f git@github.com:axoft1/test-tsk-prof.git master:gh-pages
+
+cd -
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+export default defineConfig({
+  base: "/test-task-prof/",
+});
